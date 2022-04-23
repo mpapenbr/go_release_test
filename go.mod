7dc8efa1419369130ac0e4144be62595d8a7984a
mpapenbr <Markus.Papenbrock@gmail.com>
2022-04-23 13:25:13 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mpapenbr/go_release_test
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mpapenbr/go_release_test/cmd"
+)
+
+var (
+	version = "localdev"
+)
+
+func main() {
+
+	fmt.Printf("This is Sample app LocalVersion: %s Cmd.Version: %s MyVersion: %s\n", version, cmd.GetVersion(), cmd.MyVersion)
+}
