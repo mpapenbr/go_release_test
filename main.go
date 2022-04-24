d26946570cc06cb63e985e7d97211205165f3ec6
mpapenbr <Markus.Papenbrock@gmail.com>
2022-04-24 15:20:18 +0200

    fix: another try

--- main.go
+++ main.go
@@ -14,5 +14,6 @@ func main() {
 
 	fmt.Printf("This is Sample app LocalVersion: %s Cmd.Version: %s MyVersion: %s\n", version, cmd.GetVersion(), cmd.MyVersion)
 	fmt.Printf("We can now print another line!")
+	fmt.Printf("Try again!")
 
 }
