4bda6e62a2df9e16b15572d8fbba9a8b65313780
mpapenbr <Markus.Papenbrock+github@gmail.com>
2022-04-24 15:49:04 +0000

    feat!: removed test outputs

--- main.go
+++ main.go
@@ -13,12 +13,5 @@ var (
 func main() {
 
 	fmt.Printf("This is Sample app LocalVersion: %s Cmd.Version: %s MyVersion: %s\n", version, cmd.GetVersion(), cmd.MyVersion)
-	fmt.Printf("We can now print another line!")
-	fmt.Printf("Try again!")
-	fmt.Printf("And still another try!")
-	fmt.Printf("Here we go again")
-	fmt.Printf("Yeah!!")
-	fmt.Printf("Breaking Yeah!!")
-	fmt.Printf("Another breaking Yeah!!")
 
 }
